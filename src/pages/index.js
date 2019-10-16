b0acc6f3c9981707feb315128fcabf09bd8059a1
Dmytro Izotov <izotov.dima@gmail.com>
2019-10-16 20:36:30 +0300

    Create structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react"
+import "typeface-roboto"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Button from "@material-ui/core/Button"
+import styles from "./../styles/index.module.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Link to="/page-2/">
+        <div title="ENTER THE VOID">ENTER THE VOID</div>
+      </Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
