43b7e5933e08579b02ea658827c2db76f89dcb19
Dmytro Izotov <izotov.dima@gmail.com>
2019-10-21 10:20:36 +0300

    Clean warnings

--- src/components/entrance.js
+++ src/components/entrance.js
@@ -1,84 +1,84 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styles from "./../styles/entrance.module.scss"
-const Entrance = ({}) => (
-  <div className={styles.Entrance}>
-    <Link to="/page-2/">
-      <div>
-        <img src="https://picsum.photos/500/500?image=503" alt="" />
-        <img src="https://picsum.photos/500/500?image=503" alt="" />
-        <p>FOLLOW THE WHITE RABBIT</p>
-      </div>
+const Entrance = ({ onClick }) => (
+  <div onClick={onClick} className={styles.Entrance}>
+    <div>
+      <img src="https://picsum.photos/500/500?image=503" alt="" />
+      <img src="https://picsum.photos/500/500?image=503" alt="" />
+      <p>FOLLOW THE WHITE RABBIT</p>
+    </div>
 
-      <svg>
-        <filter id="transmissionerror">
-          <feColorMatrix
-            type="saturate"
-            values="0"
-            in="SourceGraphic"
-            result="colormatrix1"
+    <svg>
+      <filter id="transmissionerror">
+        <feColorMatrix
+          type="saturate"
+          values="0"
+          in="SourceGraphic"
+          result="colormatrix1"
+        />
+        <feColorMatrix
+          type="matrix"
+          values="0.95 0 0 0 0  0 0 0 0 0  0 0 0.4 0 0  0 0 0 1 0"
+          in="colormatrix1"
+          result="colormatrix2"
+        />
+        <feTurbulence
+          type="turbulence"
+          baseFrequency="0.002 0.008"
+          numOctaves="2"
+          seed="5"
+          stitchTiles="noStitch"
+          result="turbulence"
+        >
+          <animate
+            attributeName="baseFrequency"
+            from="0.002 0.008"
+            to="0.002 0.01"
+            dur="3500ms"
+            repeatCount="indefinite"
           />
-          <feColorMatrix
-            type="matrix"
-            values="0.95 0 0 0 0  0 0 0 0 0  0 0 0.4 0 0  0 0 0 1 0"
-            in="colormatrix1"
-            result="colormatrix2"
+          <animate
+            attributeName="numOctaves"
+            from="1"
+            to="10"
+            dur="750ms"
+            repeatCount="indefinite"
           />
-          <feTurbulence
-            type="turbulence"
-            baseFrequency="0.002 0.008"
-            numOctaves="2"
-            seed="5"
-            stitchTiles="noStitch"
-            result="turbulence"
-          >
-            <animate
-              attributeName="baseFrequency"
-              from="0.002 0.008"
-              to="0.002 0.01"
-              dur="3500ms"
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="numOctaves"
-              from="1"
-              to="10"
-              dur="750ms"
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="seed"
-              from="1"
-              to="100"
-              dur="10s"
-              repeatCount="indefinite"
-            />
-          </feTurbulence>
-          <feColorMatrix
-            type="saturate"
-            values="30"
-            in="turbulence"
-            result="colormatrix3"
+          <animate
+            attributeName="seed"
+            from="1"
+            to="100"
+            dur="10s"
+            repeatCount="indefinite"
           />
-          <feColorMatrix
-            type="matrix"
-            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 100 -15"
-            in="colormatrix3"
-            result="colormatrix4"
-          />
-          <feDisplacementMap
-            in="colormatrix2"
-            in2="colomatrix4"
-            scale="15"
-            xChannelSelector="R"
-            yChannelSelector="A"
-            result="displacementMap"
-          />
-        </filter>
-      </svg>
-    </Link>
+        </feTurbulence>
+        <feColorMatrix
+          type="saturate"
+          values="30"
+          in="turbulence"
+          result="colormatrix3"
+        />
+        <feColorMatrix
+          type="matrix"
+          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 100 -15"
+          in="colormatrix3"
+          result="colormatrix4"
+        />
+        <feDisplacementMap
+          in="colormatrix2"
+          in2="colomatrix4"
+          scale="15"
+          xChannelSelector="R"
+          yChannelSelector="A"
+          result="displacementMap"
+        />
+      </filter>
+    </svg>
   </div>
 )
+Entrance.propTypes = {
+  onClick: PropTypes.func,
+}
 
 export default Entrance
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,59 @@
 import React from "react"
 import "typeface-roboto"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Entrance from "../components/entrance"
-import styles from "./../styles/index.module.scss"
+import "./../styles/index.module.scss"
+
+import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
 
 const IndexPage = () => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
   return (
     <Layout>
       <SEO title="Home" />
-      <Entrance />
+      <Entrance onClick={handleClickOpen} />
       {/* <Link to="/page-2/">W</Link> */}
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Warning"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              A very small percentage of people may experience a seizure when
+              exposed to certain visual images, including flashing lights or
+              visual patterns. Even people who have no history of seizures or
+              epilepsy may have an undiagnosed condition that can cause these
+              "photosensitive epileptic seizures" while visiting my website.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={handleClose} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     </Layout>
   )
 }
