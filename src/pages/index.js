8a56919b44430fa9e667c2684bde6e0b3365e9cf
Dmytro Izotov <izotov.dima@gmail.com>
2019-11-21 23:49:10 +0200

    add component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,28 @@ import styles from "./../styles/index.module.scss"
 import Typical from "react-typical"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Div100vh from "react-div-100vh"
 const IndexPage = () => {
   return (
-    <div className={styles.index}>
-      <SEO title="Dmytro Izotov" />
-      <Header isDark></Header>
-      <div className={styles.index__upperStick}></div>
-      <div className={styles.index__banner}>
-        <p style={{ fontWeight: "300" }}>
-          Welcome to the{" "}
-          <strong style={{ fontWeight: "600" }}>
-            Personal Internet Habitat
-          </strong>{" "}
-          of Dmytro Izotov
-        </p>
-      </div>
-      <div className={styles.index__lowerStick}></div>
-    </div>
+    <Div100vh>
+      <main>
+        <div className={styles.index}>
+          <SEO title="Dmytro Izotov" />
+          <Header isDark></Header>
+          <div className={styles.index__upperStick}></div>
+          <div className={styles.index__banner}>
+            <p style={{ fontWeight: "300" }}>
+              Welcome to the{" "}
+              <strong style={{ fontWeight: "600" }}>
+                Personal Internet Habitat
+              </strong>{" "}
+              of Dmytro Izotov
+            </p>
+          </div>
+          <div className={styles.index__lowerStick}></div>
+        </div>
+      </main>
+    </Div100vh>
   )
 }
 
