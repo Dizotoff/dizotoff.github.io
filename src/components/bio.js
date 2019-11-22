dd7f9f15afb548ed9710b70ebf853a135ae62673
Dmytro Izotov <izotov.dima@gmail.com>
2019-11-22 10:40:17 +0200

    test styles rendering

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 import styles from "../styles/bio.module.scss"
+import { Link } from "gatsby"
 import mypic from "../../content/assets/profile-pic.jpg"
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -23,7 +24,7 @@ const Bio = () => {
       }
     }
   `)
-
+  const linkStyle = { color: "#deeaf3", cursor: "pointer" }
   const { author, linkedin } = data.site.siteMetadata
   return (
     <div className={styles.bio}>
@@ -35,8 +36,7 @@ const Bio = () => {
       <p></p>
       <p className={styles.bio__description}>
         Written by <strong>{author}</strong> who lives and works in Helsinki
-        building cool things. You should add him at{" "}
-        <a href={linkedin}>Linkedin</a>
+        building cool things. You should find him at Linkedin
       </p>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,23 @@ import Header from "../components/header"
 import Div100vh from "react-div-100vh"
 const IndexPage = () => {
   return (
-    <Div100vh className={styles.index}>
-      <SEO title="Dmytro Izotov" />
-      <Header isDark></Header>
-      <div className={styles.index__upperStick}></div>
-      <div className={styles.index__banner}>
-        <p style={{ fontWeight: "300" }}>
-          Welcome to the{" "}
-          <strong style={{ fontWeight: "600" }}>
-            Personal Internet Habitat
-          </strong>{" "}
-          of Dmytro Izotov
-        </p>
-      </div>
-      <div className={styles.index__lowerStick}></div>
-    </Div100vh>
+    <div className={styles.easyIn}>
+      <Div100vh className={styles.index}>
+        <SEO title="Dmytro Izotov" />
+        <Header isDark></Header>
+        <div className={styles.index__upperStick}></div>
+        <div className={styles.index__banner}>
+          <p style={{ fontWeight: "300" }}>
+            Welcome to the{" "}
+            <strong style={{ fontWeight: "600" }}>
+              Personal Internet Habitat
+            </strong>{" "}
+            of Dmytro Izotov
+          </p>
+        </div>
+        <div className={styles.index__lowerStick}></div>
+      </Div100vh>
+    </div>
   )
 }
 
