4a771a8c0ec3ea13883fd1448e5f8eeaa9cfc6bb
Dmytro Izotov <izotov.dima@gmail.com>
2019-11-22 22:21:49 +0200

    add form

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,6 +37,17 @@ const About = () => (
         </p>
         <p>In my spare time I like to do different fun things with numbers.</p>
         <p>Life is good.</p>
+        <form
+          name="contact-form"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input name="name" placeholder="Your Name" type="text" />
+          <input name="email" placeholder="name@name.com" type="email" />
+          <textarea name="message" />
+          <button>Send</button>
+        </form>
       </div>
     </div>
   </Layout>
