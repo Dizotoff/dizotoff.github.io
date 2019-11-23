32dda92e611ad347da46d52e2975ac78c9d82694
Dmytro Izotov <izotov.dima@gmail.com>
2019-11-23 02:46:56 +0200

    edit about" "

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,30 +13,34 @@ const About = () => (
     <div className={styles.about}>
       <Header></Header>
       <div className={styles.about__content}>
-        <h1>Welcome Internet</h1>
-        <p>
-          Welcome to my humble online residence where you may come and rest for
-          no reason.
-        </p>
-        <p>
-          I'm 22 years old developer currently residing in Helsinki, Finland.
-          The city where I moved to get my bachelor's degree in software
-          engineering. During my studies, I had a lucky chance to do an exchange
-          semester at Mount Royal University in Canada.
-        </p>
-        <p>
-          Currently, I'm working in Telia company where I develop web
-          interfaces. I specialize in front end development with an emphasis on
-          UX design and accessibility.
-        </p>
-        <p>
-          I was born in Kamianets-Podilskyi city in Ukraine. It's a quite small
-          city with mild winter and hot summer. After high school, I had been
-          studying physics for two years at Taras Shevchenko National University
-          of Kyiv.
-        </p>
-        <p>In my spare time I like to do different fun things with numbers.</p>
-        <p>Life is good.</p>
+        <article>
+          <center>
+            <h1>Welcome Internet</h1>
+          </center>
+          <p>
+            Welcome to my humble online residence where you may come and rest
+            for as long as you pleased.
+          </p>
+          <p>
+            I'm 22 years old developer currently residing in Helsinki, Finland.
+            The city where I moved to get my bachelor's degree in software
+            engineering. Subsequently, I tried founding a couple of startups.
+          </p>
+          <p>
+            During my studies, I had a lucky chance to do an exchange semester
+            at Mount Royal University in Canada. I have been working as web
+            developer in Telia since I came back.
+          </p>
+          <p>
+            I was born in Kamianets-Podilskyi city in Ukraine. It's a quite
+            small city with mild winter and hot summer. After high school, I had
+            been studying physics at Taras Shevchenko National University of
+            Kyiv.
+          </p>
+          <p>Parties and calculus are my best skills.</p>
+          <p>Life is good.</p>
+          <p>❤️</p>
+        </article>
       </div>
     </div>
   </Layout>
