ef98342b30e8a6131719c68de9b94c1b130153b5
Dmytro Izotov <izotov.dima@gmail.com>
2022-04-29 01:12:43 +0300

    Update text

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -9,12 +9,9 @@ export default class DataService {
         icon: "info_bubble",
         content: {
           paragraphs: [
-            "Hi, I am Dmytro :)",
+            "Hi, I am Dmytro and I am a full-stack developer :)",
             "I was born in a Kamianets-Podilskyi, a small town with mild winter and hot summer in the western part of Ukraine.",
-            "Currently, I'm a developer residing in Helsinki, Finland. The city where I moved to get my bachelor's degree in software engineering. I've been working as a full-time web developer since the midpoint of my studies and successfully finished my degree",
-            "During my studies, I had a lucky chance to do an exchange semester at Mount Royal University in Canada.",
-            "My main tech stack is React & Node.js. Nowdays, I do full-stack developing for Telia",
-            "",
+            "Currently, I'm residing in Helsinki, Finland and building the future of work in blockchain startup Kleoverse.",
             "Life is good ♥",
           ],
         },
@@ -27,27 +24,34 @@ export default class DataService {
           resumeLink: "/",
           workExperience: [
             {
-              jobTitle: "Web Developer",
+              jobTitle: "Full Stack Developer",
+              company: "Kleoverse",
+              location: "Metaverse",
+              period: "JAN 2022 - NOW",
+              accomplishments: ["Web3", "Blockchain"],
+            },
+            {
+              jobTitle: "Full Stack Developer",
               company: "Telia",
               location: "Helsinki, Finland",
-              period: "MAY 2019 - NOW",
-              accomplishments: ["eCommerce microservices", "Design System"],
+              period: "MAY 2019 - JAN 2022",
+              accomplishments: ["eCommerce", "Design System", "Microservices"],
             },
           ],
           education: [
             {
-              credit: "Bachelor in Software Engineering",
+              credit: "Bachelor in Software Engineering (100% degree finished)",
               place:
                 "Helsinki Metropolia University of Applied Sciences, Finland",
               period: "SEPT 2016 – DEC 2020",
             },
             {
-              credit: "Bachelor in Computer Science (Exchange Student)",
+              credit: "Bachelor in Computer Science (Exchange student)",
               place: "Mount Royal University, Canada",
               period: "SEPT 2018 – DEC 2018",
             },
             {
-              credit: "Bachelor in Applied Physics",
+              credit: "Bachelor in Applied Physics (50% degree finished)",
               place: "Taras Shevchenko National University of Kyiv, Ukraine",
               period: "SEPT 2014 – AUG 2016",
             },
@@ -61,69 +65,29 @@ export default class DataService {
         content: {
           projects: [
             {
-              title: "WarmChat",
+              title: "Zero-Zenith",
               description:
-                "Mobile friendly web chat where users can exchange using only warm messages",
-              techStack: "React, Node, Express",
-              url: "https://pacific-castle-71280.herokuapp.com",
+                "Hiking route to every single country's highest peak from sea level",
+              techStack: "Next.js, Firebase, Typescript",
+              url: "https://www.zero-zenith.com/",
             },
             {
-              title: "Photo-Beats",
-              description: "Real-time audio reactive visuals in the browser",
-              techStack: "React, Gatsby",
-              repo: "https://github.com/Dizotoff/Photo-Beats",
-              url: "https://photo-beats.com/",
-            },
-  {
               title: "Monqrime",
-              description: "An ecommerce store assembled for a friend within 12 hours of work. It is fully functioning, go order some stuff!",
+              description:
+                "An ecommerce store assembled for a friend within 12 hours of work. It is fully functioning, go order some stuff!",
               techStack: "Shopify",
               url: "https://monqrime.com/",
             },
             {
               title: "My Portfolio Site",
               description: "Personal website",
-              techStack: "React,",
+              techStack: "React, Gatbsy",
               repo: "https://github.com/dizotoff/",
             },
           ],
         },
       },
-      {
-        id: "skills",
-        name: "Skills.txt",
-        icon: "progman_11",
-        content: {
-          hard: [
-            {
-              name: "Node.js",
-              progress: 100,
-            },
-            {
-              name: "React",
-              progress: 100,
-            },
-            {
-              name: "Scrum",
-              progress: 100,
-            },
-            {
-              name: "Javascript",
-              progress: 90,
-            },
-            {
-              name: "GraphQL",
-              progress: 80,
-            },
-            {
-              name: "Jest",
-              progress: 80,
-            },
-             
-          ],
-          soft: "Teamwork, Leadership, Analytical thinking",
-        },
-      },
+
       {
         id: "contact",
         name: "Contact.txt",
@@ -131,7 +95,7 @@ export default class DataService {
         content: {
           emailText:
             "If you want to hire me or invite to a project, just email me on ",
-          email: "izotov.dima@gmail.com",
+          email: "dmytro.eth@proton.me",
           socialText: "Or you can reach me out through social media:",
           social: [
             {
@@ -145,7 +109,7 @@ export default class DataService {
 
             {
               name: "FaTwitter",
-              link: "https://twitter.com/dizotoff",
+              link: "https://twitter.com/dmytro_eth",
             },
           ],
         },
